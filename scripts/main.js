d8ccfa82fd40ce2763c0d80396577eb5c0fd3b5e
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2019-06-20 22:08:14 -0400

    Major changes

---
+++ scripts/main.js
@@ -0,0 +1,166 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }  
+});
+
+particlesJS("particles-js", {
+  "particles": {
+      "number": {
+          "value": 88,
+          "density": {
+              "enable": true,
+              "value_area": 700
+          }
+      },
+      "color": {
+          "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+      },
+      "shape": {
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 15
+          }
+      },
+      "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+              "enable": false,
+              "speed": 1.5,
+              "opacity_min": 0.15,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 2.5,
+          "random": false,
+          "anim": {
+              "enable": true,
+              "speed": 2,
+              "size_min": 0.15,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "distance": 110,
+          "color": "#33b1f8",
+          "opacity": 0.25,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 1.6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+          }
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": false,
+              "mode": "repulse"
+          },
+          "onclick": {
+              "enable": false,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "grab": {
+              "distance": 400,
+              "line_linked": {
+                  "opacity": 1
+              }
+          },
+          "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 200,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
+          }
+      }
+  },
+  "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
